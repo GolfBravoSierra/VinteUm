9d494c6494bd8de5ff2b1e39c8e66a1821790ff7
GolfBravoSierra <106974254+GolfBravoSierra@users.noreply.github.com>
2023-09-05 14:54:59 -0300

    05/09/2023

--- src/VinteUm.java
+++ src/VinteUm.java
@@ -1,5 +1,4 @@
-public class VinteUm {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+public class VinteUm
+{
+
+} // end class VinteUm
